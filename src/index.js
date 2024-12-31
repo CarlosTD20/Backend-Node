5abd2ef8952b8537268dab1098a7100b45bd959e
Carlos Tornero <carlostornero204@gmail.com>
2024-12-31 21:26:00 +0100

    Añdir Cache

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
-
 const bodyParser = require('body-parser')
+const apicahce = require('apicache')
+
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 
 const app = express()
+const cache = apicahce.middleware
 const PORT = process.env.PORT || 3000
 
-// app.get("/", (req, res) => {
-//     res.send("<h2>It's Working!<h2>")
-// })
-
 app.use(bodyParser.json())
+app.use(cache('2 minutes'))
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () =>{
